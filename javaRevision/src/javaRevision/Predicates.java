c449db9da7cff362d271890f64bb1be84441038c
igalna <igalna@gmail.com>
2015-06-06 17:19:43 +0100

    More fun with predicates
    Also fun with the optional class Optional

--- javaRevision/src/javaRevision/Predicates.java
+++ javaRevision/src/javaRevision/Predicates.java
@@ -2,6 +2,7 @@ package javaRevision;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -24,14 +25,32 @@ public class Predicates {
 	
 	public static void main(String[] args) {
 		
+		// generates a list of the numbers 1 - 50
 		List<Integer> range = IntStream.rangeClosed(0, 50)
 			    .boxed().collect(Collectors.toList());
 		
 		System.out.println(range.stream().reduce((x,y) -> x + y).get());
 		
 		System.out.println(range.stream().filter(greaterThan(25)).collect(Collectors.<Integer>toList()));
+		System.out.println(range.stream().filter((x) -> x > 25).collect(Collectors.<Integer>toList()));
+		
+		System.out.println(range.stream().filter(lessThan(25)).collect(Collectors.<Integer>toList()));
+		System.out.println(range.stream().filter((x) -> x < 25).collect(Collectors.<Integer>toList()));
+		
+		System.out.println(range.stream().filter(divisibleBy(3)).collect(Collectors.<Integer>toList()));
+		System.out.println(range.stream().filter((x) -> x % 3 == 0).collect(Collectors.<Integer>toList()));
 		
 		
+		// Optional things
 		
+		// generates an Optional by summing up the contents of range
+		Optional opt = range.stream().reduce((x,y) -> x + y);
+		Optional opt2 = Optional.empty();
+		Optional opt3 = Optional.of(10);
+		System.out.println(opt);
+		System.out.println(opt.isPresent());
+		System.out.println(opt.get());
+		System.out.println(opt2.orElse("Nothing Here"));
+		System.out.println(opt3.get());
 	}
 }
